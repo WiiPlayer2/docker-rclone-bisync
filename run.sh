fee7e1babe4e084462a59c7f854975d7f878380d
Waldemar Tomme <waldetomme@gmail.com>
2019-11-21 05:27:16 +0100

    Lock file will be removed if exists on startup

--- run.sh
+++ run.sh
@@ -2,11 +2,16 @@
 CONFIG_FOLDER="/config"
 DATA_FOLDER="/data"
 CONFIG_FILE="$CONFIG_FOLDER/rclone.conf"
+LOCK_FILE="/sync.lock"
 export RCLONE_CONFIG="$CONFIG_FILE"
 
 if [ ! -f "$CONFIG_FILE" ]; then
     echo "Creating rclone config \"$REMOTE_NAME\" of type \"$REMOTE_TYPE\""
     rclone config create "$REMOTE_NAME" "$REMOTE_TYPE" config_is_local false
+fi
+
+if [ -e "$LOCK_FILE" ]; then
+    rm -f $LOCK_FILE
 fi
 
 while true; do
